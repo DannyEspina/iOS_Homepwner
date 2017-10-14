41ccdeded18cb077d7f2178363fd601ac427d715
Daniel Espina <dannyj.espina@gmail.com>
2017-10-14 16:17:28 -0400

    finished chapter 10

---
+++ Homepwner/ItemStore.swift
@@ -0,0 +1,26 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Danny Espina on 10/14/17.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+}
