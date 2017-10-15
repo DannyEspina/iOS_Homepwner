2885cf3403d84a6fe037413b55fcba6a460d663b
Daniel Espina <dannyj.espina@gmail.com>
2017-10-15 17:40:31 -0400

    Finished Chapter 11 w/ challeges

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -10,8 +10,84 @@ import UIKit
 
 class ItemsViewController: UITableViewController {
     var itemStore: ItemStore!
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+    
+    @IBAction func addNewItem(_ sender: UIButton) {
+        // Create a new item and add it to the store
+        let newItem = itemStore.createItem()
+        
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.index(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            
+            // Insert this new row into the table
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        // If you are currently in editing mode...
+        if isEditing {
+            // Change text of button to inform user of state
+            sender.setTitle("Edit", for: .normal)
+            
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        } else {
+            // Change text of button to inform user of state
+            sender.setTitle("Done", for: .normal)
+            
+            // enter editing mode
+            setEditing(true, animated: true)
+        }
+    }
+    override func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        if (proposedDestinationIndexPath.row >= itemStore.allItems.count){
+            return sourceIndexPath
+        }
+        return proposedDestinationIndexPath
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //Adding the row for no more elements string
+        return itemStore.allItems.count + 1
+    }
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        if (indexPath.row >= itemStore.allItems.count){
+            return false
+        }
+        return true
+    }
+    override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "Remove"
+    }
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath,
+                            to destinationIndexPath: IndexPath) {
+
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+        
+    }
+    override func tableView(_ tableView: UITableView, commit editingStyle:UITableViewCellEditingStyle,
+                            forRowAt indexPath: IndexPath) {
+        // If the table view is asking to commit a delete command...
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            let deleteAction = UIAlertAction(title: "Remove", style: .destructive,
+                                             handler: { (action) -> Void in
+            // Remove the item from the store
+            self.itemStore.removeItem(item)
+            
+            // Also remove that row from the table view with an animation
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+            ac.addAction(deleteAction)
+            // Present the alert controller
+            present(ac, animated: true, completion: nil)
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,38 +104,27 @@ class ItemsViewController: UITableViewController {
         imageView.contentMode = .scaleAspectFit
         tableView.backgroundView = imageView
     }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return itemStore.allItems.count
-        default:
-            return 1
-        }
-        
-    }
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.section < 1 {
+        if indexPath.row < itemStore.allItems.count {
             return 60
         } else {
             return 44   // For "No more items!"
         }
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Get a new or recycled cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
-        cell.backgroundColor = UIColor.clear
-        // Set the text on the cell with the description of the item
-        // that is at the nth index of items, where n = row this cell
-        // will appear in on the tableview
-        let item = itemStore.allItems[indexPath.row]
-        if indexPath.section < 1 {
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-        } else {
-            cell.textLabel?.text = "No more items!"
-            cell.detailTextLabel?.text = ""
-        }
-        return cell
+        if indexPath.row < itemStore.allItems.count{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell",for: indexPath )
+            let item = itemStore.allItems[indexPath.row]
+            cell.textLabel?.text = item.name
+            cell.detailTextLabel?.text = "$" + String(item.valueInDollars)
+            cell.backgroundColor = UIColor.clear
+            return cell;
+        }else{
+            let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell_NMI")
+            cell.textLabel?.text = "No more Items!"
+            cell.backgroundColor = UIColor.clear
+            return cell
             
+        }
     }
 }
